ac7640ad23d2144a4dcf15c1d32d305ffa63be52
mentipento <26530260+mentipento@users.noreply.github.com>
2025-01-09 13:31:28 +0100

    wrap all variables in factory functions

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
-const rows = 3;
-const columns = 3;
-const board = [];
+function Gameboard() {
 
-    for (let i = 0; i < rows; i++) {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
       board[i] = [];
       for (let j = 0; j < columns; j++) {
-        board[i].push(0);
+          board[i].push(0);
       }
-    }
+  }
 
+  return {
+      board
+  }
 
-let playerOneName = "Peter";
-let playerTwoName = "Klaus"
-
-const players = [
-        {
-          name: playerOneName,
-          token: 1
-        },
-        {
-          name: playerTwoName,
-          token: 2
-        }
-      ];
-
-
-
-let activePlayer = playerOneName
-
-const switchActivePlayer = () => {
-  activePlayer === playerOneName ? activePlayer = playerTwoName : activePlayer = playerOneName
 }
 
-      const selectField = (row, column) => {
-        board[row][column] = (activePlayer === players[0].name) ? players[0].token : players[1].token;
-        switchActivePlayer();
-        return board;
+function Players() {
+
+  let playerOneName = "Peter";
+  let playerTwoName = "Klaus"
+
+  const players = [{
+          name: playerOneName,
+          token: 1
+      },
+      {
+          name: playerTwoName,
+          token: 2
       }
+  ];
+
+  let activePlayer = playerOneName
+
+  const getActivePlayer = () => activePlayer;
+
+  const switchActivePlayer = () => {
+      activePlayer === playerOneName ? activePlayer = playerTwoName : activePlayer = playerOneName
+  }
+
+  return {
+      players,
+      getActivePlayer,
+      switchActivePlayer
+  }
+
+}
+
+function GameController() {
+
+  const board = Gameboard();
+  const players = Players();
+  const selectField = (row, column) => {
+      board.board[row][column] = (players.getActivePlayer() === players.players[0].name) ? players.players[0].token : players.players[1].token;
+      players.switchActivePlayer();
+      return board.board;
+  }
+  return {
+      selectField
+  }
+}
+
+const game = GameController()
